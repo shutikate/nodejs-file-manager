58c34b8d54cb8b642f5153912a5c3afe3e3f1d78
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2024-10-09 14:21:52 +0200

    feat: add close subtask

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'node:url';
 import readline from 'node:readline/promises';
 import { EOL } from 'node:os';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const getNameOfUser = () => {
   const passedArgs = process.argv.slice(2);
@@ -12,13 +16,24 @@ const getNameOfUser = () => {
 
 const runFileManager = async () => {
 
-  const greetingMessage = `Welcome to the File Manager, ${getNameOfUser()}!`
+  const username = getNameOfUser();
+  const greetingMessage = `Welcome to the File Manager, ${username}!`;
+  const closeMessage = `Thank you for using File Manager, ${username}, goodbye!`;
   process.stdout.write(`${greetingMessage}${EOL}${EOL}`);
 
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
+  rl.setPrompt(`You are currently in ${__dirname}${EOL}`);
+  rl.on('line', (data) => {
+    if (data.toString() === '.exit') {
+      rl.close();
+    } else {
+      rl.prompt();
+    }
+  });
+  rl.on('close', () => process.stdout.write(`${closeMessage}${EOL}`));
 
 }
 
